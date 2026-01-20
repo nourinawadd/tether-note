dc4923a16527821a86cc80da3f351638feff0f05
Nourin <nourinawad@gmail.com>
2026-01-20 23:21:43 +0200

    initialize project

---
+++ app.js
@@ -0,0 +1,10 @@
+import 'dotenv/config';
+import express from 'express';
+
+const app = express();
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+});
+
+export default app;

401f53020d917ec035ff740baedbee557d956769
Nourin <nourinawad@gmail.com>
2026-01-23 14:48:01 +0200

    chore: implement authorization middleware'

---
+++ backend/middleware/auth.middleware.js
@@ -0,0 +1,33 @@
+import 'dotenv/config.js';
+import jwt from 'jsonwebtoken';
+import User from '../models/user.model';
+
+const authorize = async (req, res, next) => {
+    try {
+        let token;
+        if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+            token = req.headers.authorization.split(' ')[1];
+        }
+
+        if(!token) {
+            const error = new Error('No token provided, authorization denied.');
+            res.status = 401;
+            throw error;
+        }
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const user = await User.findById(decoded.userId).select('-password');
+        if(!user) return res.status(401).json({ success: false, message: 'Unauthorized '});
+
+        req.user = user;
+        next();
+    }
+    catch(e) {
+        res.status(401).json({
+            success: false,
+            message: 'Unauthorized!',
+            error: e.message
+        });
+    }
+}
+
+export default authorize;
